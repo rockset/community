9fbee6aaeaed2058cda19fa74212da6593467c49
Nadine Farah <nfarah86@gmail.com>
2020-05-15 14:54:41 -0700

    removed files from renaming

--- tutorials/rockset-mongo-integration/mongo_config.py
+++
@@ -1,9 +0,0 @@
-
-from pymongo import MongoClient
-from settings import MONGO_URI
-
-
-client = MongoClient(MONGO_URI)
-db = client.get_database("weather_pollution_db")
-weather_data_collection = db.weather_data
-pollution_data_collection = db.air_pollution_data
--- tutorials/rockset-mongo-integration/sql-files/current_weather.sql
+++
@@ -1,5 +0,0 @@
-select w.observation_time.value, w.temp
-from commons.weather_data_collection as w
-where w.observation_time.value is not NULL
-order by w.observation_time.value DESC
-limit 1;
